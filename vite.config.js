564d39eb35e7ee945d0bf7ccad521965056debe9
EmmyBod <emmanuelolatunbosun020@gmail.com>
2026-08-16 11:16:23 -0700

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 100000000,
   },
-  base: '/ai_automation/',
+  base: process.env.NODE_ENV === "production"
+    ? "/ai_automation/"
+    : "/",
 });
